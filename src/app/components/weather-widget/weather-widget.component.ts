bba7c547b14eb020d950146728858474ff0dacf9
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2024-09-06 09:37:54 +0300

    Using templates to render content conditionally

--- src/app/components/weather-widget/weather-widget.component.ts
+++ src/app/components/weather-widget/weather-widget.component.ts
@@ -1,10 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, ContentChild, signal, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-weather-widget',
   templateUrl: './weather-widget.component.html',
-  styleUrls: ['./weather-widget.component.scss']
+  styleUrls: ['./weather-widget.component.scss'],
 })
 export class WeatherWidgetComponent {
-  hidden = signal<boolean>(false)
+  hidden = signal<boolean>(false);
+
+  @ContentChild('footerWidget') footerContent: TemplateRef<any> | null = null;
 }
